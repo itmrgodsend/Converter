db0a63ab762774bed4cb9e1b109b5148cd53c0d9
itmrgodsend <itmrgodsend@gmail.com>
2020-08-17 12:44:35 +0300

    Add Engine

--- src/Rate/Rate.jsx
+++ src/Rate/Rate.jsx
@@ -3,12 +3,50 @@ import React, {Component} from 'react';
 
 
 class Rate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'date': '',
+            'currencyRate': {}
+        }
+        this.currency = ['USD', 'RUB', 'CAD', 'PHP']
+        this.getRate();
+    }
+
+    getRate = () => {
+        fetch('https://api.exchangeratesapi.io/latest')
+            .then(data => {
+                return data.json()
+            })
+            .then(data => {
+                this.setState({date: data.date})
+                let result = {};
+                for (let i = 0; i < this.currency.length; i++) {
+                    result[this.currency[i]] = data.rates[this.currency[i]]
+                }
+                console.log(result);
+                this.setState({currencyRate: result})
+            })
+    }
+
     render() {
         return (
+
+
             <div className='main'>
-                <div className='blocks'>1</div>
-                <div className='blocks'>2</div>
-                <div className='blocks'>3</div>
+                <h3 className='date'>Курс на {this.state.date}</h3>
+                <div className='rate'>
+
+                        {Object.keys(this.state.currencyRate).map((keyName, i) =>
+                            (
+                                <div key={keyName} className='blocks'>
+                                    <div>{keyName}</div>
+                                    <div>{this.state.currencyRate[keyName]}</div>
+                                </div>
+                            )
+                        )}
+
+                </div>
             </div>
         );
     }
