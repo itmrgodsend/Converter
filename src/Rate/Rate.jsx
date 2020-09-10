680a7b00053539c44dad3f7dfb3e9afb7fb86139
itmrgodsend <itmrgodsend@gmail.com>
2020-09-10 16:07:29 +0300

    Finish chart engine

--- src/Rate/Rate.jsx
+++ src/Rate/Rate.jsx
@@ -2,7 +2,7 @@ import classes from './Rate.module.css'
 import React, {Component, useEffect, useState} from 'react';
 import Calc from "../Calc/Calc";
 import {Line} from "react-chartjs-2";
-import ChartUSD from "./ChartUSD";
+
 
 
 const Rate = (props) => {
@@ -10,20 +10,54 @@ const Rate = (props) => {
     for (let key in props.rate.data) {
         ArrayData.push(key)
     }
-    const [chartData, setChartData] = useState({});
+    const [chartDataUSD, setChartDataUSD] = useState({});
+    const [chartDataEUR, setChartDataEUR] = useState({});
+    const [chartDataCAD, setChartDataCAD] = useState({});
 
     const chart = () => {
-        let Arr = Object.entries(props.rate.data);
-        let ArrayRate = [];
-        for (let i = 0; i < Arr.length; i++) {
-            ArrayRate.push((1 / Arr[i][1].USD).toFixed(3))
+        let ArrUSD = Object.entries(props.rate.data);
+        let ArrayRateUSD = [];
+        for (let i = 0; i < ArrUSD.length; i++) {
+            ArrayRateUSD.push((1 / ArrUSD[i][1].USD).toFixed(3))
+        }
+        let ArrEUR = Object.entries(props.rate.data);
+        let ArrayRateEUR = [];
+        for (let i = 0; i < ArrEUR.length; i++) {
+            ArrayRateEUR.push((1 / ArrEUR[i][1].EUR).toFixed(3))
+        }
+        let ArrCAD = Object.entries(props.rate.data);
+        let ArrayRateCAD = [];
+        for (let i = 0; i < ArrCAD.length; i++) {
+            ArrayRateCAD.push((1 / ArrCAD[i][1].CAD).toFixed(3))
         }
 
-        setChartData({
+        setChartDataUSD({
             labels: ArrayData.sort(),
             datasets: [{
                 label: "Rate",
-                data: ArrayRate.sort(),
+                data: ArrayRateUSD.sort(),
+                backgroundColor: [
+                    'rgba(75,192,192,0.6)'
+                ],
+                borderWidth: 4
+            }]
+        })
+        setChartDataEUR({
+            labels: ArrayData.sort(),
+            datasets: [{
+                label: "Rate",
+                data: ArrayRateEUR.sort(),
+                backgroundColor: [
+                    'rgba(75,192,192,0.6)'
+                ],
+                borderWidth: 4
+            }]
+        })
+        setChartDataCAD({
+            labels: ArrayData.sort(),
+            datasets: [{
+                label: "Rate",
+                data: ArrayRateCAD.sort(),
                 backgroundColor: [
                     'rgba(75,192,192,0.6)'
                 ],
@@ -46,20 +80,24 @@ const Rate = (props) => {
             <div className='rate'>
 
 
-                <ChartUSD Chart={props.rate}/>
+                <div key={props.rate.currencyRate} className='blocks'>
+                    <div>USD</div>
+                    <div>{(1 / props.rate.currencyRate.USD).toFixed(3)}</div>
+                    <div><Line data={chartDataUSD}/></div>
+                </div>
                 <div key={props.rate.currencyRate} className='blocks'>
                     <div>EUR</div>
                     <div>{(1 / props.rate.currencyRate.EUR).toFixed(3)}</div>
-                    <div><Line data={chartData}/></div>
+                    <div><Line data={chartDataEUR}/></div>
                 </div>
                 <div key={props.rate.currencyRate} className='blocks'>
                     <div>CAD</div>
                     <div>{(1 / props.rate.currencyRate.CAD).toFixed(3)}</div>
-                    <div><Line data={chartData}/></div>
+                    <div><Line data={chartDataCAD}/></div>
                 </div>
 
 
-                <Line data={chartData}/>
+
             </div>
         </div>
     );
